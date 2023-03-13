1b37393f8913ed352cb8c3d7a01ed950522e3d06
Denis Osipov <d.osipov@contactpay.com>
2023-03-13 12:53:25 +0300

    fix tests for circle

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,30 +1,39 @@
-from math import pi
-
 import pytest
 
 from src.Circle import Circle
 from src.Square import Square
 
 
-class TestCircle:
-    """Tests for class Circle."""
-
-    radius = 1.5
+@pytest.mark.parametrize(
+    'radius, area, perimeter, figure, common_area',
+    [
+        (1, 3.14, 6.28, Square(1), 4.14),
+        (1.5, 7.07, 9.42, Square(1.55), 9.47),
+    ]
+)
+def test_base_attrs(
+        radius: float,
+        area: float,
+        perimeter: float,
+        figure: object,
+        common_area: float,
+):
     circle = Circle(radius)
+    assert circle.name == 'Circle'
+    assert circle.radius == radius
+    assert round(circle.area, 2) == area
+    assert round(circle.perimeter, 2) == perimeter
+    assert round(circle.add_area(figure), 2) == common_area
 
-    def test_base_attrs(self):
-        assert self.circle.name == 'Circle'
-        assert self.circle.radius == self.radius
 
-    def test_area(self):
-        assert self.circle.area == pi * self.radius * self.radius
+@pytest.mark.parametrize('radius', [0, -1, 'str'])
+def test_negative_creation(radius: float):
+    with pytest.raises(ValueError):
+        Circle(radius)
 
-    def test_perimeter(self):
-        assert self.circle.perimeter == (2 * pi * self.radius)
 
-    def test_add_area(self):
-        side = 1.5
-        square = Square(side)
-        assert self.circle.add_area(square) == (pi * self.radius * self.radius + side * side)
-        with pytest.raises(ValueError):
-            self.circle.add_area(side)
+@pytest.mark.parametrize('non_figure', [0, 1, -1, 'str'])
+def test_negative_add_area(non_figure: object):
+    circle = Circle(1)
+    with pytest.raises(ValueError):
+        circle.add_area(non_figure)
