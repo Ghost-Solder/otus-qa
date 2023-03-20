b7d5769db85d28ce120e26441d13cc191dbf2cc6
Denis Osipov <d.osipov@contactpay.com>
2023-03-20 21:54:50 +0300

    add name-main idiom

--- script.py
+++ script.py
@@ -13,42 +13,47 @@ BOOKS_CSV_PATH = get_path('books.csv')
 USERS_JSON_PATH = get_path('users.json')
 
 
-with (
-    open(BOOKS_CSV_PATH, 'r', newline='') as books_file,
-    open(USERS_JSON_PATH, 'r', newline='') as users_file,
-):
-    books = DictReader(books_file)
-    users = json.load(users_file)
+def distribute_to_users(books_file_path: str, users_file_path: str):
+    with (
+        open(books_file_path, 'r', newline='') as books_file,
+        open(users_file_path, 'r', newline='') as users_file,
+    ):
+        books = DictReader(books_file)
+        users = json.load(users_file)
 
-    users_iter = iter(users)
+        users_iter = iter(users)
 
-    result = {}
+        result = {}
 
-    for book in books:
-        try:
-            user = next(users_iter)
-        except StopIteration:
-            users_iter = iter(users)
-            user = next(users_iter)
+        for book in books:
+            try:
+                user = next(users_iter)
+            except StopIteration:
+                users_iter = iter(users)
+                user = next(users_iter)
 
-        book_data = {
-            'title': book['Title'],
-            'author': book['Author'],
-            'pages': book['Pages'],
-            'genre': book['Genre'],
-        }
-
-        if not result.get(user['name']):
-            result[user['name']] = {
-                'name': user['name'],
-                'gender': user['gender'],
-                'address': user['address'],
-                'age': user['age'],
-                'books': [book_data],
+            book_data = {
+                'title': book['Title'],
+                'author': book['Author'],
+                'pages': book['Pages'],
+                'genre': book['Genre'],
             }
-        else:
-            result[user['name']]['books'].append(book_data)
 
-    with open('result.json', 'w') as result_file:
-        s = json.dumps(list(result.values()), indent=4)
-        result_file.write(s)
+            if not result.get(user['name']):
+                result[user['name']] = {
+                    'name': user['name'],
+                    'gender': user['gender'],
+                    'address': user['address'],
+                    'age': user['age'],
+                    'books': [book_data],
+                }
+            else:
+                result[user['name']]['books'].append(book_data)
+
+        with open('result.json', 'w') as result_file:
+            s = json.dumps(list(result.values()), indent=4)
+            result_file.write(s)
+
+
+if __name__ == '__main__':
+    distribute_to_users(BOOKS_CSV_PATH, USERS_JSON_PATH)
