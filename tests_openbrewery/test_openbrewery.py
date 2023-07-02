0e872250fcdd95c42c2315ddf71149d7d543f1c6
Denis Osipov <d.osipov@contactpay.com>
2023-07-02 14:57:30 +0300

    tests for openbrewery

--- tests_openbrewery/test_openbrewery.py
+++ tests_openbrewery/test_openbrewery.py
@@ -19,29 +19,29 @@ def validate_response(response: 'Response', schema: dict, number: Optional[int] 
 class TestsOpenBreweryApi:
     """Some positive tests for Open Brewery Api https://www.openbrewerydb.org/documentation"""
 
-    openbrewery_api_url = 'https://api.openbrewerydb.org/v1/breweries'
+    brewery_api_url = 'https://api.openbrewerydb.org/v1/breweries'
 
-    schema = {
+    brewery_schema = {
         'type': 'array',
         'items': {
             'type': 'object',
             'properties': {
-                "id": {'type': 'string'},
-                "name": {'type': 'string'},
-                "brewery_type": {'type': 'string'},
-                "address_1": {'type': ['null', 'string']},
-                "address_2": {'type': ['null', 'string']},
-                "address_3": {'type': ['null', 'string']},
-                "city": {'type': 'string'},
-                "state_province": {'type': 'string'},
-                "postal_code": {'type': 'string'},
-                "country": {'type': 'string'},
-                "longitude": {'type': ['null', 'string']},
-                "latitude": {'type': ['null', 'string']},
-                "phone": {'type': 'string'},
-                "website_url": {'type': ['null', 'string']},
-                "state": {'type': 'string'},
-                "street": {'type': 'string'},
+                'id': {'type': ['null', 'string']},
+                'name': {'type': 'string'},
+                'brewery_type': {'type': 'string'},
+                'address_1': {'type': ['null', 'string']},
+                'address_2': {'type': ['null', 'string']},
+                'address_3': {'type': ['null', 'string']},
+                'city': {'type': 'string'},
+                'state_province': {'type': 'string'},
+                'postal_code': {'type': 'string'},
+                'country': {'type': 'string'},
+                'longitude': {'type': ['null', 'string']},
+                'latitude': {'type': ['null', 'string']},
+                'phone': {'type': ['null', 'string']},
+                'website_url': {'type': ['null', 'string']},
+                'state': {'type': 'string'},
+                'street': {'type': ['null', 'string']},
             },
         },
     }
@@ -49,5 +49,29 @@ class TestsOpenBreweryApi:
     @pytest.mark.parametrize('number', [1, 3, 20])
     def test_breweries_list(self, number: int):
         payload = {'per_page': number}
-        response = requests.get(f'{self.openbrewery_api_url}', params=payload)
-        validate_response(response, self.schema)
+        response = requests.get(self.brewery_api_url, params=payload)
+        validate_response(response, self.brewery_schema)
+
+    @pytest.mark.parametrize('state', ['California', 'New York'])
+    def test_get_breweries_by_state(self, state: str):
+        payload = {'by_state': state}
+        response = requests.get(self.brewery_api_url, params=payload)
+        validate_response(response, self.brewery_schema)
+
+    @pytest.mark.parametrize('city', ['San Francisco', 'Los Angeles'])
+    def test_get_breweries_by_city(self, city):
+        payload = {'by_state': city}
+        response = requests.get(self.brewery_api_url, params=payload)
+        validate_response(response, self.brewery_schema)
+
+    @pytest.mark.parametrize('brewery_type', ['micro', 'nano'])
+    def test_get_breweries_by_type(self, brewery_type):
+        payload = {'by_state': brewery_type}
+        response = requests.get(self.brewery_api_url, params=payload)
+        validate_response(response, self.brewery_schema)
+
+    @pytest.mark.parametrize('postal_code', ['94102', '10001'])
+    def test_get_breweries_by_postal_code(self, postal_code):
+        payload = {'by_state': postal_code}
+        response = requests.get(self.brewery_api_url, params=payload)
+        validate_response(response, self.brewery_schema)
