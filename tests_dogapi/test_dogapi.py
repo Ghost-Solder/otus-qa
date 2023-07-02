7b807a0295b17729ced077e2f6bf64d2217e5eaa
Denis Osipov <d.osipov@contactpay.com>
2023-07-02 13:41:35 +0300

    add func for validate

--- tests_dogapi/test_dogapi.py
+++ tests_dogapi/test_dogapi.py
@@ -1,7 +1,19 @@
+from typing import TYPE_CHECKING, Optional
+
 import pytest
 import requests
 
 from jsonschema import validate
+
+if TYPE_CHECKING:
+    from requests import Response
+
+
+def validate_response(response: 'Response', schema: dict, number: Optional[int] = None) -> None:
+    assert response.status_code == 200
+    validate(instance=response.json(), schema=schema)
+    if number is not None:
+        assert len(response.json()['message']) == number
 
 
 class TestsDogApi:
@@ -239,73 +251,52 @@ class TestsDogApi:
 
     def test_breeds_list(self):
         response = requests.get(f'{self.dogapi_url}/breeds/list/all')
-        assert response.status_code == 200
-        validate(instance=response.json(), schema=self.schema)
+        validate_response(response, self.schema)
 
     def test_single_random_image(self):
         response = requests.get(f'{self.dogapi_url}/breeds/image/random')
-        assert response.status_code == 200
-        validate(instance=response.json(), schema=self.schema)
+        validate_response(response, self.schema)
 
-    @pytest.mark.parametrize('number', [0, 1, 20, 50])
+    @pytest.mark.parametrize('number', [1, 20, 50])
     def test_multiple_random_images(self, number: int):
         response = requests.get(f'{self.dogapi_url}/breeds/image/random/{number}')
-        assert response.status_code == 200
-        validate(instance=response.json(), schema=self.schema)
-        # посчитать количество картинок, которые вернулись
+        validate_response(response, self.schema, number)
 
     @pytest.mark.parametrize('breed', [
         'affenpinscher',
     ])
     def test_all_images_by_breed(self, breed: str):
         response = requests.get(f'{self.dogapi_url}/breed/{breed}/images')
-        assert response.status_code == 200
-        validate(instance=response.json(), schema=self.schema)
+        validate_response(response, self.schema)
 
-    @pytest.mark.parametrize('number', [0, 1, 20, 50])
+    @pytest.mark.parametrize('number', [1, 20, 50])
     @pytest.mark.parametrize('breed', [
         'affenpinscher',
     ])
     def test_images_by_breed(self, number: int, breed: str):
         url = f'{self.dogapi_url}/breed/{breed}/images/random'
-        if number:
-            response = requests.get(f'{url}/{number}')
-            assert response.status_code == 200
-            validate(instance=response.json(), schema=self.schema)
-        else:
-            response = requests.get(f'{url}')
-            assert response.status_code == 200
-            validate(instance=response.json(), schema=self.schema)
-        # посчитать количество картинок, которые вернулись
+        response = requests.get(f'{url}/{number}')
+        validate_response(response, self.schema, number)
 
     @pytest.mark.parametrize('breed', [
         'affenpinscher',
     ])
     def test_subbreed_list(self, breed):
         response = requests.get(f'{self.dogapi_url}/breed/{breed}/list')
-        assert response.status_code == 200
-        validate(instance=response.json(), schema=self.schema)
+        validate_response(response, self.schema)
 
     @pytest.mark.parametrize('breed, subbreed', [
         ('hound', 'afghan'),
     ])
     def test_all_images_by_subbreed(self, breed: str, subbreed: str):
         response = requests.get(f'{self.dogapi_url}/breed/{breed}/{subbreed}/images')
-        assert response.status_code == 200
-        validate(instance=response.json(), schema=self.schema)
+        validate_response(response, self.schema)
 
-    @pytest.mark.parametrize('number', [0, 1, 20, 50])
+    @pytest.mark.parametrize('number', [1, 20, 50])
     @pytest.mark.parametrize('breed, subbreed', [
         ('hound', 'afghan'),
     ])
     def test_all_images_by_subbreed(self, breed: str, subbreed: str, number: int):
         url = f'{self.dogapi_url}/breed/{breed}/{subbreed}/images/random'
-        if number:
-            response = requests.get(f'{url}/{number}')
-            assert response.status_code == 200
-            validate(instance=response.json(), schema=self.schema)
-        else:
-            response = requests.get(f'{url}')
-            assert response.status_code == 200
-            validate(instance=response.json(), schema=self.schema)
-    # посчитать количество картинок, которые вернулись
+        response = requests.get(f'{url}/{number}')
+        validate_response(response, self.schema, number)
