0c7f940598f4e4a6b7a2cf74c965c639403b8b61
Denis Osipov <d.osipov@contactpay.com>
2023-07-04 21:43:52 +0300

    first test

--- tests/test_opencart.py
+++ tests/test_opencart.py
@@ -1,23 +1,22 @@
 from selenium.webdriver.common.by import By
 
 
-# def test_example(browser):
-#     browser.find_element(By.XPATH, "//input[@name='ya']")
-
-
 class TestOpenCart:
 
-    def test_main_page(self):
+    def test_main_page(self, browser):
+        browser.find_element(By.XPATH, '//img[@title="Your Store"]')
         pass
 
-    def test_catalog_page(self):
+    def test_catalog_page(self, browser):
         pass
 
-    def test_product_page(self):
+    def test_product_page(self, browser):
         pass
 
-    def test_login_page(self):
+    def test_login_to_admin_page(self, browser):
+        browser.get(f'{browser.url}/admin')
         pass
 
-    def test_registration_page(self):
+    def test_registration_page(self, browser):
+        browser.get(f'{browser.url}/index.php?route=account/register')
         pass
