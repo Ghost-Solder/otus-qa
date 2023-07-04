41660c9401751e6d4ccf092ab0905b36ed6fe55f
Denis Osipov <d.osipov@contactpay.com>
2023-07-04 21:32:04 +0300

    temp

--- tests/test_opencart.py
+++ tests/test_opencart.py
@@ -1,6 +1,23 @@
 from selenium.webdriver.common.by import By
 
 
-def test_example(browser):
-    element = browser.find_element(By.XPATH, "//input[@name='ya']")
-    assert element.is_displayed()
+# def test_example(browser):
+#     browser.find_element(By.XPATH, "//input[@name='ya']")
+
+
+class TestOpenCart:
+
+    def test_main_page(self):
+        pass
+
+    def test_catalog_page(self):
+        pass
+
+    def test_product_page(self):
+        pass
+
+    def test_login_page(self):
+        pass
+
+    def test_registration_page(self):
+        pass
