d4d07bcf382b77384d28a6a1d158f369c606254b
Denis Osipov <d.osipov@contactpay.com>
2023-07-04 21:25:49 +0300

    add right url for opencart

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from selenium.webdriver.safari.options import Options as SafariOptions
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='chrome',
                      help='Browser name (firefox, chrome, opera, safari, edge, yandex)')
-    parser.addoption('--url', action='store', default='https://ya.ru',
+    parser.addoption('--url', action='store', default='https://demo.opencart.com/',
                      help='Base URL for the tests')
 
 
